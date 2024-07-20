3b376e0d7e0a9642aa0e1a2904f8ef7f92383b80
todd-estes <elestes@gmail.com>
2024-07-20 19:28:29 -0400

    Add 404 response

--- app/server.rb
+++ app/server.rb
@@ -9,8 +9,16 @@ loop do
   request = client_socket.gets
   puts "Received Request: #{request}"
 
-  response = "HTTP/1.1 200 OK\r\n\r\n"
-  puts "Response: #{response}"
+  request_line = request.split(" ")[1]
+  puts "Request Line: #{request_line}"
+
+  if request_line == "/"
+    response = "HTTP/1.1 200 OK\r\n\r\n"
+    puts "OK Response: #{response}"
+  else 
+    response = "HTTP/1.1 404 Not Found\r\n\r\n"
+    puts "Bad Response: #{response}"
+  end
 
   client_socket.puts response
   client_socket.close
