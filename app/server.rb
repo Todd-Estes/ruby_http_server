8134d99f450264dd1952727618e9a1c5e68ffcfd
todd-estes <elestes@gmail.com>
2024-07-22 19:56:15 -0400

    Enable multiple clients/concurrent connections

--- app/server.rb
+++ app/server.rb
@@ -4,41 +4,41 @@ require "socket"
 server = TCPServer.new("localhost", 4221)
 
 loop do
-  client_socket, client_address = server.accept
+  Thread.start(server.accept) do |client_socket, client_address| 
+    request = client_socket.gets.chomp
+    puts "Received Request: #{request}"
 
-  request = client_socket.gets
-  puts "Received Request: #{request}"
+    request_line = request.split(" ")[1]
+    puts "Request Line: #{request_line}"
 
-  request_line = request.split(" ")[1]
-  puts "Request Line: #{request_line}"
+    split_request_line = request_line.split("/")
+    puts "Split Request Line: #{split_request_line}"
 
-  split_request_line = request_line.split("/")
-  puts "Split Request Line: #{split_request_line}"
+    while line = client_socket.gets
+      break if line == "\r\n"
+      if line.start_with?("User-Agent")
+        user_agent_value = line.split(" ").last
+      end
+    end    
 
-  while line = client_socket.gets
-    break if line == "\r\n"
-    if line.start_with?("User-Agent")
-      user_agent_value = line.split(" ").last
+
+    if split_request_line.empty?
+      response = "HTTP/1.1 200 OK\r\n\r\n"
+      puts "OK Response: #{response}"
+    elsif split_request_line[1] == "user-agent"
+      response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: #{user_agent_value.length}\r\n\r\n#{user_agent_value}"
+      puts "/user-agent endpoint response: #{response}"
+    elsif split_request_line[1] == "echo" && !split_request_line[2].empty?
+      text= split_request_line.last
+      response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: #{text.length}\r\n\r\n#{text}"
+      puts response
+    else
+      response = "HTTP/1.1 404 Not Found\r\n\r\n"
+      puts "Bad Response: #{response}"
     end
-  end    
 
-
-  if split_request_line.empty?
-    response = "HTTP/1.1 200 OK\r\n\r\n"
-    puts "OK Response: #{response}"
-  elsif split_request_line[1] == "user-agent"
-    response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: #{user_agent_value.length}\r\n\r\n#{user_agent_value}"
-    puts "/user-agent endpoint response: #{response}"
-  elsif split_request_line[1] == "echo" && !split_request_line[2].empty?
-    text= split_request_line.last
-    response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: #{text.length}\r\n\r\n#{text}"
-    puts response
-  else
-    response = "HTTP/1.1 404 Not Found\r\n\r\n"
-    puts "Bad Response: #{response}"
+    client_socket.puts response
+    client_socket.close
   end
-
-  client_socket.puts response
-  client_socket.close
 end
 
